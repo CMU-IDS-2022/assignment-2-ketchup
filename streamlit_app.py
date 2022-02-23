07aa9906eba7e70a7a59f8da06226b7f978647be
Erin Lin <89028282+err1nn@users.noreply.github.com>
2022-02-23 18:25:00 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,100 +1,126 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from PIL import Image
 
-st.title("How many couples sleep in separate beds?👩‍❤️‍👨 👩‍❤️‍👩 👨‍❤️‍👨")
-st.markdown("Data source: FiveThirtyEight [Sleeping alone data](https://github.com/fivethirtyeight/data/tree/master/sleeping-alone-data)")
+st.title("Do Couples Always Sleep Together? 👩‍❤️‍👨 👩‍❤️‍👩 👨‍❤️‍👨")
+
+image = Image.open('Figs/sleeping_together_or_not.png')
+st.image(image, caption='Photo by Somnox Sleep on Unsplash')
+
+st.subheader("Data")
+st.markdown("Source: [Sleeping alone data](https://github.com/fivethirtyeight/data/tree/master/sleeping-alone-data) by FiveThirtyEight")
 #@st.cache  # add caching so we load the data only once
+
+# ===================================== PART 0 =====================================
+# Read data
+
 def load_data(url, encode = 'utf-8'):
     return pd.read_csv(url, encoding = encode, index_col=0)
 
 url_raw = 'https://raw.githubusercontent.com/fivethirtyeight/data/22a478af3edc00f69693c3f5f4604b2f1fd024b0/sleeping-alone-data/sleeping-alone-data.csv'
-url_separate = 'https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-ketchup/master/Sleep_separately.csv'
-url_together = 'https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-ketchup/master/sleep_together.csv'
-url_demo = 'https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-ketchup/master/demographic_data.csv'
+url_separate = 'https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-ketchup/master/Data/sleep_separately.csv'
+url_together = 'https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-ketchup/master/Data/sleep_together.csv'
+url_demo = 'https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-ketchup/master/Data/demographic_data.csv'
 
-# READ DATA
 raw = load_data(url_raw, encode ='ISO-8859-1')
 demo_df = load_data(url_demo)
 separate_df = load_data(url_separate)
 together_df = load_data(url_together)
 
 with st.expander("See raw data"):
-    st.text('Here are the raw data')
+    st.write('Here is the raw data.')
     st.write(raw)
 
 with st.expander("See cleaned data"):
-    st.write("Here are the cleaned data")
+    st.write("Here is the cleaned data.")
     st.text("Demographic data:")
     st.write(demo_df)
-    st.text("Data of couples who sleep separately:")
-    st.write(separate_df)
     st.text("Data of couples who sleep together:")
     st.write(together_df)
+    st.text("Data of couples who sleep separately:")
+    st.write(separate_df)
 
 
-#===================================== PART 1 =================================================
+# MAIN CODE
+
+
+# ===================================== PART 1 =====================================
 # Basic information
 # Sleep together or not?
-st.title("Let's see how many couples sleep together")
-demo_df['Sleep Together?'] = demo_df['sleep together?'].map({0: 'NO', 1:'YES'})
+st.header("Part 1: Sleep Together or Not?")
+st.write("We have 1079 valid responses. Let's look at how many couples actually sleep together every day.")
 
-st.text("After removing all null values, we have total 1078 data.")
-sleep_together = alt.Chart(demo_df).mark_bar(tooltip = True, size = 70).encode(
-    alt.X('count()'),
-    alt.Y('Sleep Together?:N', sort='-x', axis=alt.Axis(title='Do couples sleep together?')),
+demo_df['Sleep Together?'] = demo_df['sleep together?'].map({0: 'NO', 1:'YES'})
+sleep_together = alt.Chart(demo_df, title='Do you and your partner sleep together every day?').mark_bar(tooltip = True, size = 30).encode(
+    alt.X('count()', axis=alt.Axis(title='')),
+    alt.Y('Sleep Together?:N', sort='-x', axis=alt.Axis(title='')),
     alt.Color('Sleep Together?', scale=alt.Scale(
             domain=['YES', 'NO'],
-            range=['pink', 'lightblue']))
+            range=['pink', 'lightblue']), legend=None)
 ).interactive(
-).properties(width=600, height=300)
+).properties(width=600, height=200)
 st.write(sleep_together)
 
+st.write("Surprise! Among the 1079 respondent, there are 493 of them who do not always sleep with their partner.")
+st.write("Now, you may wonder, among those who don't always sleep with their partner, are they married?\
+         How long have they been in the relationship with their partner?")
+
+# Sleep together or not?
+group = st.radio("👇 Select one of the demographics you want to look at",
+     ('Relationship Status', 'Relationship Length'))
 
 # Relationship status
-st.title('How the relationships status look like')
 status_chart = alt.Chart(demo_df, title='Relationship Status').mark_bar(tooltip = True).encode(
-    x = alt.X('count()'),
-    y = alt.Y('CurrentRelationshipStatus', sort = '-x'), 
+    x = alt.X('count()', axis=alt.Axis(title='')),
+    y = alt.Y('CurrentRelationshipStatus', sort = '-x', axis=alt.Axis(title='')), 
     color=alt.Color('Sleep Together?',
             scale=alt.Scale(
             domain=['YES', 'NO'],
             range=['pink', 'lightblue']))
-).properties(
-    width = 600
-)
-st.write(status_chart)
-
+).properties(width = 600)
+    
 # Timespan of current relationship
+categories_in_order = ["More than 20 years", "16-20 years", "11-15 years", "6-10 years", "1-5 years", "Less than 1 year"]
 length_chart = alt.Chart(demo_df, title='Relationship Length').mark_bar(tooltip = True).encode(
-    x = alt.X('count()'),
-    y = alt.Y('RelationshipLength', sort = '-x'),
+    x = alt.X('count()', axis=alt.Axis(title='')),
+    y = alt.Y('RelationshipLength', sort = categories_in_order, axis=alt.Axis(title='')),
     color=alt.Color('Sleep Together?',
             scale=alt.Scale(
             domain=['YES', 'NO'],
             range=['pink', 'lightblue']))
-).properties(
-    width = 600
-)
-st.write(length_chart)
+).properties(width = 600)
 
-# Frequency of sleeping separately
-freq_chart = alt.Chart(demo_df, title='Frequency in separate beds').mark_bar(tooltip = True).encode(
-    x = alt.X('count()'),
-    y = alt.Y('Frequency in separate beds', sort = ['Never', 'Once a year or less', 'Once a month or less', 'A few times per month', 'A few times per week', 'Every night']),
-    color=alt.Color('Sleep Together?',
-        scale=alt.Scale(
-        domain=['YES', 'NO'],
-        range=['pink', 'lightblue']))
-).properties(
-    width = 600
-)
-st.write(freq_chart)
+if group == 'Relationship Status':
+     st.write(status_chart)
+else:
+     st.write(length_chart)
+
+# freq_chart = alt.Chart(demo_df, title='Frequency in separate beds').mark_bar(tooltip = True).encode(
+#     x = alt.X('count()'),
+#     y = alt.Y('Frequency in separate beds', sort = ['Never', 'Once a year or less', 'Once a month or less', 'A few times per month', 'A few times per week', 'Every night']),
+#     color=alt.Color('Sleep Together?',
+#         scale=alt.Scale(
+#         domain=['YES', 'NO'],
+#         range=['pink', 'lightblue']))
+# ).properties(
+#     width = 600
+# )
+# st.write(freq_chart)
+
+
+# ===================================== PART 2 =====================================
+# Correlations between frequency of sleeping separately and demographics
+
+st.header("Part 2: Demographics Correlations")
+st.write("""The survey collected demographic data of the respondents, including age, gender, household income, and education level.
+         Demographic data is useful for researcher to understand human behavior, and it can be used in many ways to learn the generalities of a particular population.""")
+st.write("The demographic distributions can help us better understand the correlation between **demographics** and **whether the couple sleep together**.")
 
 # Demographic data
-age_chart = alt.Chart(demo_df, title='Age').mark_bar(tooltip = True).encode(
+age_chart = alt.Chart(demo_df).mark_bar(tooltip = True).encode(
     x = alt.X('count()'),
-    y = alt.Y('Age', sort = ['18-29', '30-44', '45-60', '> 60', 'Did not disclose']),
+    y = alt.Y('Age', sort = ['> 60', '45-60', '30-44', '18-29', 'Did not disclose']),
     color=alt.Color('Sleep Together?',
         scale=alt.Scale(
         domain=['YES', 'NO'],
@@ -103,7 +129,7 @@ age_chart = alt.Chart(demo_df, title='Age').mark_bar(tooltip = True).encode(
     width = 600
 )
 
-gender_chart = alt.Chart(demo_df, title='Gender').mark_bar(tooltip = True).encode(
+gender_chart = alt.Chart(demo_df).mark_bar(tooltip = True).encode(
     x = alt.X('count()'),
     y = alt.Y('Gender', sort = '-x'),
     color=alt.Color('Sleep Together?',
@@ -114,7 +140,7 @@ gender_chart = alt.Chart(demo_df, title='Gender').mark_bar(tooltip = True).encod
     width = 600
 )
 
-income_chart = alt.Chart(demo_df, title='Household Income').mark_bar(tooltip = True).encode(
+income_chart = alt.Chart(demo_df).mark_bar(tooltip = True).encode(
     x = alt.X('count()'),
     y = alt.Y('Household income', sort = ['$150,000+', '$100,000 - $149,999', '$50,000 - $99,999', '$25,000 - $49,999', '$0 - $24,999', 'Did not disclose']),
     color=alt.Color('Sleep Together?',
@@ -124,7 +150,8 @@ income_chart = alt.Chart(demo_df, title='Household Income').mark_bar(tooltip = T
 ).properties(
     width = 600
 )
-education_chart = alt.Chart(demo_df, title='Education Level').mark_bar(tooltip = True).encode(
+    
+education_chart = alt.Chart(demo_df).mark_bar(tooltip = True).encode(
     x = alt.X('count()'),
     y = alt.Y('Education', sort = ['Graduate degree', 'Bachelor degree', 'Some college or Associate degree', 'High school degree', 'Less than high school degree', 'Did not disclose']),
     color=alt.Color('Sleep Together?',
@@ -135,39 +162,49 @@ education_chart = alt.Chart(demo_df, title='Education Level').mark_bar(tooltip =
     width = 600
 )
 
-location_chart = alt.Chart(demo_df, title='Location').mark_bar(tooltip = True).encode(
-    x = alt.X('count()'),
-    y = alt.Y('Location', sort = '-x'),
-    color=alt.Color('Sleep Together?',
-            scale=alt.Scale(
-            domain=['YES', 'NO'],
-            range=['pink', 'lightblue']))
-).properties(
-    width = 600
-)
+# location_chart = alt.Chart(demo_df).mark_bar(tooltip = True).encode(
+#     x = alt.X('count()'),
+#     y = alt.Y('Location', sort = '-x'),
+#     color=alt.Color('Sleep Together?',
+#             scale=alt.Scale(
+#             domain=['YES', 'NO'],
+#             range=['pink', 'lightblue']))
+# ).properties(
+#     width = 600
+# )
 
 # Drop down list for user to select demographic features
 demo_options = st.multiselect(
-     'What demographic feature distribution do you want to see?',
-     ['Age', 'Gender', 'Household Income', 'Education', 'Location'])
+     '👇 Select one or more demographic factors you are interested in',
+     ['Age', 'Gender', 'Household Income', 'Education'])
 if 'Age' in demo_options:
     st.write(age_chart)
 if 'Gender' in demo_options:
     st.write(gender_chart)
 if 'Education' in demo_options:
     st.write(education_chart)
-if 'Location' in demo_options:
-    st.write(location_chart)
 if 'Household Income' in demo_options:
     st.write(income_chart)
-if 'Location' in demo_options:
-    st.write(location_chart)
+#if 'Location' in demo_options:
+#    st.write(location_chart)
+    
+# ===================================== PART 3 =====================================
+# Drop down demographics and timespan of relationship selection + reasons (bar chart)
+# Select the reasons for sleeping in separate beds and show the correspondent demographics data
 
+st.header("Part 3: Why Couples Do Not Sleep Together?")
 
-#====================================== PART 4 =================================================
+# ===================================== PART 4 =====================================
 # Drop down reason selection + questions opinions (box plots)
-# Select the reasons for sleeping in separate beds and show the correspondent opinion 
-st.title('The reason of sleeping separately and their opinions')
+# Select the reasons for sleeping in separate beds and show the correspondent opinion
+
+st.header("Part 4: Is Sleeping Separately Better?")
+st.write("""In addition to investigating the reasons for sleeping separately,
+         the survey also asked those who don't sleep with their partner for their opinions of the following three questions.""")
+st.markdown("- Sleeping in separate beds helps us to stay together")
+st.markdown("- We sleep better when we sleep in separate beds")
+st.markdown("- Our sex life has improved as a result of sleeping in separate beds")
+
 separate_reasons =['Reason_One of us snores',
                 'Reason_One of us makes frequent bathroom trips in the night',
                 'Reason_One of us is sick','Reason_We are no longer physically intimate',
@@ -180,7 +217,7 @@ questions = ['Sleeping in separate beds helps us to stay together', 'We sleep be
              'Our sex life has improved as a result of sleeping in separate beds' ]
 
 reason_options = st.radio(
-    "Select the reason you want to see",
+    "👇 Select the reason you want to see",
     ['One of us snores','One of us makes frequent bathroom trips in the night',
     'One of us is sick','We are no longer physically intimate',
     'We have different temperature preferences for the room',
@@ -247,6 +284,7 @@ st.markdown("This project was created by Li-Hsin Lin and Kylie Hsieh for the [In
 
 
 # brushes
+sleep_together_brush = alt.selection_multi(fields=['sleep together?'])
 status_brush = alt.selection_multi(fields=['CurrentRelationshipStatus'])
 length_brush = alt.selection_multi(fields=['RelationshipLength'])
 freq_brush = alt.selection_multi(fields=['Frequency in separate beds'])
